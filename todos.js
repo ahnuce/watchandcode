fae35488bba95370ac6cae7d9b80f64b45917a14
Anusone Nanthavongsa <ahnuce@gmail.com>
2017-07-15 11:20:32 -0400

    more on customizing functions 2.3

---
+++ todos.js
@@ -0,0 +1,24 @@
+function sayHiTo(person){
+  console.log('hi', person);
+}
+sayHiTo('gordon');
+
+
+
+
+
+
+// makeSandwichWith _____
+//   Get one slice of bread.
+//   Add ________.
+//   Put a slice of bread on top.
+//
+//   function makeSandwichWith(filling){
+//     Get one slice of bread;
+//     Add filling;
+//     Put a slice of bread on top;
+//   }
+//
+//   makeSandwichWith(ham)
+//
+//   console.log()
