98f8a3a041a89117fa7cd66800b91cd30bbbef9f
Anusone Nanthavongsa <ahnuce@gmail.com>
2017-07-15 11:47:33 -0400

    version 2 done

--- todos.js
+++ todos.js
@@ -1,13 +1,15 @@
-function sayHiTo(person){
-  console.log('hi', person);
-}
-sayHiTo('gordon');
+//store todos
+// var todos = ["item1", "item2", "item3"]
+// //display todos
+// console.log(todos)
+// //add new todos
+// todos.push('new todo')
+// //change todos
+// todos[1] = 'changed'
+// //delete todos
+// todos.splice(0, 1)
 
-
-
-
-
-
+//version 2
 // makeSandwichWith _____
 //   Get one slice of bread.
 //   Add ________.
@@ -22,3 +24,36 @@ sayHiTo('gordon');
 //   makeSandwichWith(ham)
 //
 //   console.log()
+
+
+// function sayHiTo(person){
+//   console.log('hi', person);
+// }
+// sayHiTo('gordon');
+
+
+//display todo function
+var todos = ['item1', 'item2', 'item3']
+function displayTodos(){
+  console.log(todos);
+}
+displayTodos();
+
+//add todo function
+function addTodo(todo){
+  todos.push(todo);
+  displayTodos();
+}
+addTodo('newer todo');
+
+//change todo function
+function changeTodo(index, newValue){
+  todos[index] = newValue;
+  displayTodos();
+}
+
+//delete todos function
+function deleteTodos(index){
+  todos.splice(index, 1);
+  displayTodos();
+}
