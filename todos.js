089101ecd4e5224461e8d917fbfec851020bb7e1
Anusone Nanthavongsa <ahnuce@gmail.com>
2017-07-17 22:40:03 +0000

    version 3 changetodo method

--- todos.js
+++ todos.js
@@ -33,27 +33,58 @@
 
 
 //display todo function
-var todos = ['item1', 'item2', 'item3']
-function displayTodos(){
-  console.log(todos);
-}
-displayTodos();
+// var todos = ['item1', 'item2', 'item3']
+// function displayTodos(){
+//   console.log(todos);
+// }
+// displayTodos();
 
-//add todo function
-function addTodo(todo){
-  todos.push(todo);
-  displayTodos();
-}
-addTodo('newer todo');
+// //add todo function
+// function addTodo(todo){
+//   todos.push(todo);
+//   displayTodos();
+// }
+// addTodo('newer todo');
 
-//change todo function
-function changeTodo(index, newValue){
-  todos[index] = newValue;
-  displayTodos();
-}
+// //change todo function
+// function changeTodo(position, newValue){
+//   todos[position] = newValue;
+//   displayTodos();
+// }
 
-//delete todos function
-function deleteTodos(index){
-  todos.splice(index, 1);
-  displayTodos();
-}
+// //delete todos function
+// function deleteTodos(position){
+//   todos.splice(position, 1);
+//   displayTodos();
+// }
+
+//version 3
+// var myComputer = {
+//     operatingSystem: 'mac',
+//     screenSize: '15 inches',
+//     purchaseYear: 2011
+// };
+
+// var gordon = {
+//   name: 'Gordon',
+//   sayName: function(){
+//     console.log(this.name);
+//   }
+// }
+// gordon.sayName();
+
+var todoList = {
+    todos: ['item1', 'item2', 'item3'],
+    displayTodos: function(){
+        console.log('My Todos', this.displayTodos);
+    },
+    addTodo: function(todo){
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue){
+        this.todos[position] = newValue;
+        this.displayTodos();
+    }
+};
+
