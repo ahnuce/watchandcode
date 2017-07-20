c93a1ee7eb7ebe24e59b70ac1e406718109fe78d
Anusone Nanthavongsa <ahnuce@gmail.com>
2017-07-20 14:27:40 -0400

    version 4 completed

--- todos.js
+++ todos.js
@@ -33,27 +33,56 @@
 
 
 //display todo function
-var todos = ['item1', 'item2', 'item3']
-function displayTodos(){
-  console.log(todos);
-}
-displayTodos();
+// var todos = ['item1', 'item2', 'item3']
+// function displayTodos(){
+//   console.log(todos);
+// }
+// displayTodos();
+//
+// //add todo function
+// function addTodo(todo){
+//   todos.push(todo);
+//   displayTodos();
+// }
+// addTodo('newer todo');
+//
+// //change todo function
+// function changeTodo(index, newValue){
+//   todos[index] = newValue;
+//   displayTodos();
+// }
+//
+// //delete todos function
+// function deleteTodos(index){
+//   todos.splice(index, 1);
+//   displayTodos();
+// }
 
-//add todo function
-function addTodo(todo){
-  todos.push(todo);
-  displayTodos();
-}
-addTodo('newer todo');
-
-//change todo function
-function changeTodo(index, newValue){
-  todos[index] = newValue;
-  displayTodos();
-}
-
-//delete todos function
-function deleteTodos(index){
-  todos.splice(index, 1);
-  displayTodos();
-}
+//version 4
+var todoList = {
+  todos: []
+  displayTodos: function(){
+    console.log('My Todos', this.todos);
+  }
+  addTodo: function(todoText){
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  }
+  changeTodo: function(position, todoText){
+    // this.todos[position] = newValue;
+    this.todos[position].todoText = todoText
+    this.displayTodos();
+  }
+  deleteTodos: function(position){
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+  toggleCompleted: function(position){
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};
